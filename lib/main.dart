20c12896791195533cd61c8887590cf9bfc53cd8
iggy228 <i.borovy@gmail.com>
2020-12-17 09:29:06 +0100

    Setting launcher icon and locking screen on portrait

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,27 @@ import 'package:emelyst/pages/overview.dart';
 import 'package:emelyst/pages/room.dart';
 import 'package:emelyst/pages/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => Loading(),
-    '/home': (context) => Home(),
-    '/lights': (context) => Lights(),
-    '/overview': (context) => Overview(),
-    '/room': (context) => Room(),
-    '/settings': (context) => Settings(),
-  },
-));
+void main() => runApp(Emelyst());
+
+class Emelyst extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/lights': (context) => Lights(),
+        '/overview': (context) => Overview(),
+        '/room': (context) => Room(),
+        '/settings': (context) => Settings(),
+      },
+    );
+  }
+}
